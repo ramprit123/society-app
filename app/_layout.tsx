e9f6f5657155d5c87f0b07fecfd1303b3d9523c0
ramprit123 <ramprit.sahani@outlook.com>
2025-04-08 23:34:31 +0530

    feat: integrate NativeWind and Tailwind CSS for styling
    Add NativeWind and Tailwind CSS to the project to streamline styling with utility-first CSS. This includes configuring Babel, Metro, and TypeScript to support NativeWind, adding global CSS, and updating components to use Tailwind classes.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
-import { Stack } from "expo-router";
-
+import { Stack } from 'expo-router/stack';
+import '../global.css'
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
