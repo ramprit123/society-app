9c95342ab8d5c007a75323a697c9768376252b65
ramprit123 <ramprit.sahani@outlook.com>
2025-04-08 23:40:02 +0530

    feat: add Poppins font and integrate it into the app
    Add the @expo-google-fonts/poppins dependency and configure it in the app's layout. Apply the Poppins font to the text in the index.tsx file to improve the app's typography and visual consistency.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,35 @@
 import { Stack } from 'expo-router/stack';
 import '../global.css'
+import {
+  useFonts,
+  Poppins_100Thin,
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+} from '@expo-google-fonts/poppins';
+
+
 export default function RootLayout() {
+
+const [fontsLoaded] = useFonts({
+  Poppins_100Thin,
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+});
+
+if (!fontsLoaded) {
+  return null;
+}
   return <Stack screenOptions={{ headerShown: false }} />;
 }
