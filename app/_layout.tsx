b945821c7508c9c1f71060aa6f9d4e9572e204ee
ramprit123 <ramprit.sahani@outlook.com>
2025-04-09 00:11:37 +0530

    chore: add prettier config and reanimated plugin
    Add Prettier configuration file and install Prettier as a dev dependency. Also, include the react-native-reanimated plugin in the Babel config to enable animations. Update the layout component to use GestureHandlerRootView for gesture handling.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router/stack';
-import '../global.css'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import '../global.css';
 import {
   useFonts,
   Poppins_100Thin,
@@ -13,23 +14,25 @@ import {
   Poppins_900Black,
 } from '@expo-google-fonts/poppins';
 
-
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_100Thin,
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_900Black,
+  });
 
-const [fontsLoaded] = useFonts({
-  Poppins_100Thin,
-  Poppins_200ExtraLight,
-  Poppins_300Light,
-  Poppins_400Regular,
-  Poppins_500Medium,
-  Poppins_600SemiBold,
-  Poppins_700Bold,
-  Poppins_800ExtraBold,
-  Poppins_900Black,
-});
-
-if (!fontsLoaded) {
-  return null;
-}
-  return <Stack screenOptions={{ headerShown: false }} />;
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <GestureHandlerRootView>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GestureHandlerRootView>
+  );
 }
